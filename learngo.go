c1ddaa878c9fc6d62a61f40d432086dd68a84aca
Mikhail Metrikin <metrikin@gmail.com>
2017-08-01 13:19:58 -0700

    Simple functions in Go

--- learngo.go
+++ learngo.go
@@ -22,4 +22,30 @@ func main() {
   // Qualify it with the package name, fmt.
   fmt.Println("Hello World!")
 
+  // Call another function in main
+  beyondHello()
+}
+
+// Functions have parameters in parentheses.
+// If there are no parameters, empty parentheses are still required.
+func beyondHello() {
+  var x int // Var declaration. Var must be declared before it is used
+  x = 3  // Vr assingment
+
+  // "Short" declarations use := to infer the type, declare, and assign.
+  y := 4
+
+  sum, prod := learnMultiple(x, y)
+  fmt.Println("sum:", sum, "prod:", prod) // Simple output, auto deliniated
+  //learnTypes() // < y minutes, learn more!
+}
+
+/* <- multiline comment
+Functions can have parameters and (multiple!) return values.
+Here `x`, `y` are the arguments and `sum`, `prod` is the signature (what's returned).
+Note that `x` and `sum` receive the type `int`.
+*/
+
+func learnMultiple(x, y int) (sum, prod int) {
+  return x + y, x * y
 }
